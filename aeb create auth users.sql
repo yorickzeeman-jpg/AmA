05d5d7c34873f7c8b433b5a9ad268b3115d406dc
yorickzeeman-jpg <yorickzeeman@gmail.com>
2026-06-14 14:17:50 +0200

    Add files via upload

--- aeb create auth users.sql
+++ aeb create auth users.sql
@@ -1,7 +1,10 @@
-select auth.create_user(uid := gen_random_uuid(), email := 'leandre@x',   password := 'Yorick2017', email_confirm := true);
-select auth.create_user(uid := gen_random_uuid(), email := 'nokulunga@x', password := 'Yorick2017', email_confirm := true);
-select auth.create_user(uid := gen_random_uuid(), email := 'tevin@x',     password := 'Yorick2017', email_confirm := true);
-select auth.create_user(uid := gen_random_uuid(), email := 'sesi@x',      password := 'Yorick2017', email_confirm := true);
-select auth.create_user(uid := gen_random_uuid(), email := 'daleen@x',    password := 'Yorick2017', email_confirm := true);
-select auth.create_user(uid := gen_random_uuid(), email := 'mahlatse@x',  password := 'Yorick2017', email_confirm := true);
-select auth.create_user(uid := gen_random_uuid(), email := 'ithasia@x',   password := 'Yorick2017', email_confirm := true);
+insert into auth.users (id, email, encrypted_password, email_confirmed_at, created_at, updated_at, raw_app_meta_data, raw_user_meta_data)
+values
+  (gen_random_uuid(), 'yorick@x',    crypt('Yorick2017', gen_salt('bf')), now(), now(), now(), '{"provider":"email"}', '{"name":"Yorick"}'),
+  (gen_random_uuid(), 'leandre@x',   crypt('Yorick2017', gen_salt('bf')), now(), now(), now(), '{"provider":"email"}', '{"name":"Leandre"}'),
+  (gen_random_uuid(), 'nokulunga@x', crypt('Yorick2017', gen_salt('bf')), now(), now(), now(), '{"provider":"email"}', '{"name":"Nokulunga"}'),
+  (gen_random_uuid(), 'tevin@x',     crypt('Yorick2017', gen_salt('bf')), now(), now(), now(), '{"provider":"email"}', '{"name":"Tevin"}'),
+  (gen_random_uuid(), 'sesi@x',      crypt('Yorick2017', gen_salt('bf')), now(), now(), now(), '{"provider":"email"}', '{"name":"Sesi"}'),
+  (gen_random_uuid(), 'daleen@x',    crypt('Yorick2017', gen_salt('bf')), now(), now(), now(), '{"provider":"email"}', '{"name":"Daleen"}'),
+  (gen_random_uuid(), 'mahlatse@x',  crypt('Yorick2017', gen_salt('bf')), now(), now(), now(), '{"provider":"email"}', '{"name":"Mahlatse"}'),
+  (gen_random_uuid(), 'ithasia@x',   crypt('Yorick2017', gen_salt('bf')), now(), now(), now(), '{"provider":"email"}', '{"name":"Ithasia"}');
